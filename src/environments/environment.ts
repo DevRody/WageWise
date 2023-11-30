620cac29adfb1d843ef1da569106d278a2506fa6
Rodyx7 <113364044+Rodyx7@users.noreply.github.com>
2023-11-29 21:26:47 -0300

    29/11

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiURL: 'http://localhost:8080',
-//   apiURL: 'http://ec2-3-129-210-51.us-east-2.compute.amazonaws.com:8080',
+  //apiURL: 'http://localhost:8080',
+  apiURL: 'http://ec2-3-129-210-51.us-east-2.compute.amazonaws.com:8080',
 };

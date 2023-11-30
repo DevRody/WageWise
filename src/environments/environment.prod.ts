d6fe13d147a3fb2ee5de443feb76e116dfa8fca5
Rodyx7 <113364044+Rodyx7@users.noreply.github.com>
2023-11-30 12:53:54 -0300

    30/11

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiURL: 'http://ec2-3-129-210-51.us-east-2.compute.amazonaws.com:8080/',
+    apiURL: 'https://payrollsolutions-production.up.railway.app'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  //apiURL: 'http://localhost:8080',
-  apiURL: 'http://ec2-3-129-210-51.us-east-2.compute.amazonaws.com:8080',
+  // apiURL: 'http://localhost:8080',
+  apiURL: 'https://payrollsolutions-production.up.railway.app'
+  // apiURL: 'http://ec2-3-129-210-51.us-east-2.compute.amazonaws.com:8080',
 };
